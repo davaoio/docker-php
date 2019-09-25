94ec747e2e6e8ca22aa29144f1793bf809757e16
Will Fong <willfong@pm.me>
2019-09-25 17:23:33 +0800

    docker-compose with MariaDB

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
 <?php
 
   echo "Hello, I'm PHP!";
+  error_log("Log errors here");
+  $conn = new mysqli($_ENV["MYSQL_HOSTNAME"], $_ENV["MYSQL_USERNAME"], $_ENV["MYSQL_PASSWORD"], $_ENV["MYSQL_DATABASE"]);  
 
 ?>
